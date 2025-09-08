61f338cae381e14d3d59df76bd54d246a713a24c
KarmaWSYD <powertuukka@gmail.com>
2025-09-08 15:03:25 +0300

    style: remove unnecessary newline

--- db.py
+++ db.py
@@ -6,7 +6,6 @@ def _get_connection():
     conn = sqlite3.connect("database.db")
     conn.execute("PRAGMA foreign_keys ON")
     conn.row_factory = sqlite3.Row
-    
     return conn
 
 def execute(sql, params=[]):
