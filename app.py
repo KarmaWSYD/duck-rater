d4ccfa67207e015f9f76ce2508b543e95782717e
KarmaWSYD <powertuukka@gmail.com>
2025-09-21 23:19:31 +0300

    fix: change request.form () to []

--- app.py
+++ app.py
@@ -80,9 +80,9 @@ def new_duck_get():
 @app.route("/new-duck", methods=["POST"])
 def new_duck_post():
     
-    duck_name = request.form("duck-name")
-    duck_image = request.form("duck-image")
-    duck_description = request.form("duck-description")
+    duck_name = request.form["duck-name"]
+    duck_image = request.form["duck-image"]
+    duck_description = request.form["duck-description"]
     duck_description = "No description provided"
     
     require_login()
