6f2d8d2386b961077fa176348db7c794d820b1e6
KarmaWSYD <powertuukka@gmail.com>
2025-09-21 18:45:10 +0300

    feat: add .env file generation to the dotenv module if file doesn't exist

--- dotenv.py
+++ dotenv.py
@@ -2,6 +2,7 @@
 
 import os
 import logging
+import secrets
 
 def load_dotenv() -> None:
     try:
@@ -15,3 +16,21 @@ def load_dotenv() -> None:
             f.close()
     except FileNotFoundError:
         logging.warning("No .env file found") # This does not currently do much since logging hasn't been implemented across the project TODO add proper logging
+        if not os.getenv("SECRET"):
+            _generate_dotenv() # Generating a new .env file if one doesn't exist and the "SECRET" environment variable doesn't exist
+        
+def _generate_dotenv() -> None:
+    if os.path.exists("./.env"):
+        logging.error(".env file already exists! Skipping file creation")
+    else:
+        logging.info("Generating a new .env file")
+        with open("./.env/", "w") as f:
+            secret = secrets.token_hex()
+            f.write(f"SECRET={secret}")
+            os.environ["SECRET"] = secret # setting the SECRET variable if it doesn't exist
+            f.close()
+    
+    
+            
+if __name__ == "__main__":
+    load_dotenv()
