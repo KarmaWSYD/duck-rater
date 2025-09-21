34b473e81f8f4e1a202971479d91971ec2a51f9d
KarmaWSYD <powertuukka@gmail.com>
2025-09-21 20:59:43 +0300

    feat: add ducks table to sql schema

--- schema.sql
+++ schema.sql
@@ -3,3 +3,11 @@ CREATE TABLE users (
     username TEXT UNIQUE,
     password_hash TEXT
 );
+
+CREATE TABLE ducks (
+    id INTEGER PRIMARY KEY,
+    duck_name TEXT,
+    duck_image BLOB,
+    duck_description TEXT
+);
+
