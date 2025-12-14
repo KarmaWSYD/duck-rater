b70ac0b67788381164cbbbbd9e87efd691769661
KarmaWSYD <powertuukka@gmail.com>
2025-12-14 17:12:47 +0200

    fix: add missing VALUES to init.sql

--- init.sql
+++ init.sql
@@ -1,6 +1,6 @@
 DELETE FROM categories;
 
-INSERT INTO categories (name, description) ('Real Duck', 'A picture of a real duck');
-INSERT INTO categories (name, description) ('Fictional Duck', 'A picture of a fictional duck');
-INSERT INTO categories (name, description) ('Art', 'Art depicting a duck');
-INSERT INTO categories (name, description) ('Rubber Ducky', 'Rubber duck, useful for development');
+INSERT INTO categories (name, description) VALUES ('Real Duck', 'A picture of a real duck');
+INSERT INTO categories (name, description) VALUES ('Fictional Duck', 'A picture of a fictional duck');
+INSERT INTO categories (name, description) VALUES ('Art', 'Art depicting a duck');
+INSERT INTO categories (name, description) VALUES ('Rubber Ducky', 'Rubber duck, useful for development');
