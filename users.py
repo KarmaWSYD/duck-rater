a9a9c9922902a7c1ddbe096a064f073c0a00caa2
KarmaWSYD <powertuukka@gmail.com>
2026-03-30 00:04:11 +0300

    fix: prevent NoneType username causing crash on login

--- users.py
+++ users.py
@@ -1,5 +1,6 @@
 import db
 import sqlite3
+from typing import Union, Optional
 
 def create_user(username, password_hash) -> bool:
     sql = """INSERT INTO users (username, password_hash) VALUES (?, ?)"""
@@ -9,17 +10,17 @@ def create_user(username, password_hash) -> bool:
     except sqlite3.IntegrityError:
         return False
     
-def login(username) -> tuple:
+def login(username) -> Union[list, bool]:
     sql = """
         SELECT id, password_hash 
         FROM users
         WHERE username = ?
         ;"""
-    id, password_hash = db.query_one(sql, [username])
-    if not password_hash:
+    result = db.query_one(sql, [username])
+    if not result:
         return False
-    else:
-        return (id, password_hash)
+    else:        
+        return result
     
 def get_user(id):
     sql = """
@@ -29,7 +30,7 @@ def get_user(id):
     ;"""
     return db.query_one(sql, [id])
 
-def get_ducks(user):
+def get_ducks(user) -> Optional[list]:
     sql = """
         SELECT id, creator, duck_name AS title, duck_description AS description 
         FROM ducks
