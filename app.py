ca21686ea980e800e64b85c90a5f2aa7e6ebf157
KarmaWSYD <92757174+KarmaWSYD@users.noreply.github.com>
2026-04-20 00:52:11 +0300

    Add comment functionality (#5)
    * feat: add comment-related html
    
    * fix: add missing endfor block
    
    * fix: rename description to comment for comment form
    
    * feat: add and remove comments from posts
    
    * fix: incorrect reference to comments
    
    * fix: incorrect reference to comment.id instead of comment_id
    
    * fix: return comment creator user
    
    * fix: fix sql ambiguity errors
    
    * fix: jinja form ambiguity
    
    * fix: incorrect reference to comment
    
    * fix: reference to comment instead of comment_text
    
    * fix: fix sql format error
    
    * feat: make comments into divs

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from secrets import token_hex
 import random
 import os
 import items, users
+from comments import delete_comment, get_comments, add_comment
 from dotenv import load_dotenv 
 # uses locally defined dotenv.py, but should be compatible with python-dotenv
 # we're not using the proper module due to course requirements
@@ -142,6 +143,7 @@ def show_item(item_id):
     images = items.get_images(item_id)
     category = items.get_category(item["category"])
     ratings_count = items.get_ratings_count(item_id)
+    comments = get_comments(item_id=item_id)
     if ratings_count: 
         ratings_count = ratings_count[0]
     else: 
@@ -159,7 +161,7 @@ def show_item(item_id):
         user_rating = items.get_rating(item_id, session["user_id"])
     else:
         user_rating = None
-    return render_template("show_item.html", images=images, item=item, category=category, ratings_count=ratings_count, average_rating=average_rating, user_rating=user_rating)
+    return render_template("show_item.html", images=images, item=item, category=category, ratings_count=ratings_count, average_rating=average_rating, user_rating=user_rating, comments=comments)
 
 @app.route("/remove-item/<int:item_id>", methods=["GET", "POST"])
 def remove_item(item_id):
@@ -243,4 +245,24 @@ def add_rating(item_id):
     elif rating > 5:
         rating = 5
     items.add_rating(item_id, user=session["user_id"], rating=rating)
+    return redirect("/item/" + str(item_id))
+
+@app.route(rule="/create-comment/<int:item_id>", methods=["POST"])
+def create_comment(item_id):
+    require_login()
+    check_csrf()
+
+    user = session["user_id"]
+    comment = request.form["comment_text"]
+    add_comment(item_id=item_id, user_id=user, comment=comment)
+    return redirect("/item/" + str(item_id))
+
+# should this be a delete?
+@app.route(rule="/remove-comment/<int:item_id>/<int:comment_id>", methods=["POST"])
+def remove_comment(item_id, comment_id):
+    require_login()
+    check_csrf()
+    
+    user=session["user_id"]
+    delete_comment(item_id=item_id, comment_id=comment_id, user_id=user)
     return redirect("/item/" + str(item_id))
---
+++ comments.py
@@ -0,0 +1,40 @@
+import db
+
+def get_comments(item_id):
+    return db.query_all("""
+                        SELECT comments.id, comments.user_id, comments.comment AS comment_text, users.username AS user
+                        FROM comments
+                        LEFT JOIN users
+                          ON users.id = comments.user_id
+                        WHERE comments.parent_id = ?
+                        ORDER BY comments.id ASC
+                        ;""",
+                        [item_id]
+    )
+    
+def add_comment(item_id, user_id, comment) -> None:
+    db.execute("""
+               INSERT INTO comments
+               (parent_id, user_id, comment)
+               VALUES
+               (?, ?, ?)
+               ;""",
+               [item_id, user_id, comment])
+
+def edit_comment(item_id, comment_id, comment) -> None:
+    db.execute("""
+               UPDATE comments
+               SET comment = ?
+               WHERE parent_id = ?
+                 AND comment_id = ? 
+               ;"""
+               )
+        
+def delete_comment(item_id, comment_id, user_id) -> None:
+    db.execute("""
+               DELETE FROM comments
+               WHERE parent_id = ?
+                 AND comment_id = ?
+                 AND user_id = ?
+               ;""",
+               [item_id, comment_id, user_id])
